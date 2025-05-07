bc191af1c6b4de57bec1ee3e1c5b522a7f787d2e
Mihir <Mihir@LAPTOP-RJ87R4ED>
2025-05-07 15:13:54 +0530

    IndexModel

---
+++ Pages/IndexModel.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgenticAI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
